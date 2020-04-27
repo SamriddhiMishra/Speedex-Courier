315b1c2ab238deea522f8b369c7f945f347c242b
SamriddhiMishra <43378325+SamriddhiMishra@users.noreply.github.com>
2020-04-28 01:55:41 +0530

    First Commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\samriddhi mishra\Documents\Projects\SpeedEx Courier\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ env/speedex/generalzone/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render,redirect,reverse
+from . models import Enquiry,Complain,Career,LoginInfo
+import datetime
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.files.storage import FileSystemStorage
+from . import smssender
+from adminzone . models import Notification,Consignment
+# Create your views here.
+
+def index(request):
+    nf=Notification.objects.all()
+    return render(request,'index.html',{'nf':nf})
+
+def about(request):
+    nf = Notification.objects.all()
+    return render(request,'about.html',{'nf':nf})
+
+def login(request):
+    nf = Notification.objects.all()
+    return render(request,'login.html',{'nf':nf})
+
+def tracking(request):
+    nf = Notification.objects.all()
+    return render(request,'tracking.html',{'nf':nf})
+
+def complain(request):
+    nf = Notification.objects.all()
+    return render(request,'complain.html',{'nf':nf})
+
+def enquiry(request):
+    nf = Notification.objects.all()
+    return render(request,'enquiry.html',{'nf':nf})
+
+def career(request):
+    nf = Notification.objects.all()
+    if request.method=='POST' and request.FILES['myfile']:
+        name=request.POST['name']
+        gender = request.POST['gender']
+        address = request.POST['address']
+        contactno = request.POST['contactno']
+        emailaddress = request.POST['emailaddress']
+        qualification=request.POST['qualification']
+        experience=request.POST['experience']
+        keyskills=request.POST['keyskills']
+        myfile=request.FILES['myfile']
+        cv=myfile.name
+        connectdate=datetime.datetime.now().strftime('%Y/%m/%d')
+        if Career.objects.filter(emailaddress=emailaddress).exists():
+            msg='The user already registered!!'
+        else:
+            c=Career(name=name,gender=gender,address=address,contactno=contactno,emailaddress=emailaddress,qualification=qualification,experience=experience,keyskills=keyskills,cv=cv,connectdate=connectdate)
+            c.save()
+            fs=FileSystemStorage()
+            fs.save(cv,myfile)
+            msg='Your application is submitted!!'
+        return render(request,'career.html',{'msg':msg,'nf':nf})
+    return render(request,'career.html',{'nf':nf})
+
+def saveenquiry(request):
+    name=request.POST['name']
+    gender = request.POST['gender']
+    address = request.POST['address']
+    contactno = request.POST['contactno']
+    emailaddress = request.POST['emailaddress']
+    enquirytext = request.POST['enquirytext']
+    enquirydate=datetime.datetime.now().strftime('%Y/%m/%d')
+    e=Enquiry(name=name,gender=gender,address=address,contactno=contactno,emailaddress=emailaddress,enquirytext=enquirytext,enquirydate=enquirydate)
+    e.save()
+    smssender.sendsms(contactno,'Thanks for Enquiry. We will contact you soon!!! -Team HR')
+    return redirect('index')
+
+def savecomplain(request):
+    name=request.POST['name']
+    gender = request.POST['gender']
+    address = request.POST['address']
+    contactno = request.POST['contactno']
+    emailaddress = request.POST['emailaddress']
+    subject=request.POST['subject']
+    complaintext = request.POST['complaintext']
+    complaindate=datetime.datetime.now().strftime('%Y/%m/%d')
+    en=Complain(name=name,gender=gender,address=address,contactno=contactno,emailaddress=emailaddress,subject=subject,comlplaintext=complaintext,complaindate=complaindate)
+    en.save()
+    return redirect('index')
+
+def validateuser(request):
+    userid=request.POST['userid']
+    password=request.POST['password']
+    try:
+        v=LoginInfo.objects.get(userid=userid,password=password)
+        if v is not None:
+            request.session['userid']=userid
+            return redirect(reverse('adminzone:adminhome'))
+    except ObjectDoesNotExist:
+        return redirect('login')
+    #return render(request,'index.html')
+
+def search(request):
+    refno=request.POST['refno']
+    con=Consignment.objects.get(refno=refno)
+    return render(request,'tracking.html',{'con':con})
